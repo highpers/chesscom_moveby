b184462be9b643107b579fd25f4007dba2dea438
highpers <pablok@veryweb.com.ar>
2020-09-28 14:51:17 -0300

    git ignore

--- const.php
+++ const.php
@@ -1,7 +1,5 @@
 <?php
-define('TOKEN', '1341749560:AAGiV63ywSGqoh6Z5gZc6eik-OqeRK38q8');
-define('HORAS', '15');
+define('TOKEN', '1341789560:AAGiV6uxrwSGqoh6Z5gZc6eik-OqeRK38q8');
+define('HORAS', '21');
 define('TEAM' , 'club-atletico-independiente');
-define('ID_CANAL' , '-1001389hhr44962671');
-
-
+define('ID_CANAL' , '-1001389962671');
