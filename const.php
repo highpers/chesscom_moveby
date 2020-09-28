3b96fa70df1cbd3531d21eb00466cb47ea8701a4
highpers <pk130668@gmail.com>
2020-09-28 15:17:24 -0300

    borrado const.php

--- const.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('TOKEN', '1341789560:AAGiV6uxrwSGqoh6Z5gZc6eik-OqeRK38q8');
-define('HORAS', '21');
-define('TEAM' , 'club-atletico-independiente');
-define('ID_CANAL' , '-1001389962671');
